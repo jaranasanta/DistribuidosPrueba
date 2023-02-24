3ea294d0b851c851a713ed681fcf380a6f2d46a0
June <126002360+jaranasanta@users.noreply.github.com>
2023-02-24 18:19:11 +0100

    Update infor.m

--- infor.m
+++ infor.m
@@ -2,16 +2,16 @@
 clear all
 clc
 
-num=input('Introduzca un número entero:)
+num=input('Introduzca un número entero: ');
 
-factorial=1
+factorial=1;
 
 for i=1:num
-    factorial=factorial*i
+    factorial=factorial*i;
 end
 
 if num>=0
-    disp(['El factorial de',num2str(num),'es:',factorial])
+    disp(['El factorial de',num2str(num),'es:',num2str(factorial)]);
 else
-    disp(['El factorial de',num,'es:',factorial])
+    disp(['El factorial de',num2str(num),'es:',num2str(factorial)]);
 end
